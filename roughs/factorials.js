ea5a1eda5b3332278da90b87dc181405b6714782
akkie00 <akshay8801.ak@gmail.com>
2018-03-07 22:29:43 +0530

    factorial quiz

---
+++ roughs/factorials.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var solution=1;
+
+for (var i = 5; i >= 1; i--) {
+  // console.log(i);
+    solution *= i;
+  }
+  console.log(solution);
