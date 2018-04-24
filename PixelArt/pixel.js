22722694b3bb56e413d5e43e34907b38368f265f
akkie00 <akshay8801.ak@gmail.com>
2018-04-24 23:50:09 +0530

    worked on JS

--- PixelArt/pixel.js
+++ PixelArt/pixel.js
@@ -1,4 +1,16 @@
+//getting the values entered by the user
 
+var height, width, color;
+console.log("hery");
+$('#sizeOfGrid').submit(function(event){
+    console.log("hey");
+    event.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+    makeGrid(height, width);
+    console.log(height+ " is height");
+
+});
 
 function makeGrid(){
     
